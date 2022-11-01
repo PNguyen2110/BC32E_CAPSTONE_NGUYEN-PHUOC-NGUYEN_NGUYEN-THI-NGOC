75db1de31b63835c76bdba1da20bb35c89ecfcc8
NgocNguyen1998 <ngocnguyen09498@gmail.com>
2022-11-01 23:16:56 +0700

    Update BookTicket Page

--- src/pages/movieDetail/MovieDetail.jsx
+++ src/pages/movieDetail/MovieDetail.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './movieDetail.module.css'
 import { Tabs } from 'antd';
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
