423946ab9de8bb2cbbef8bec31e6ec606bf7fb84
NgocNguyen1998 <ngocnguyen09498@gmail.com>
2022-11-01 16:39:08 +0700

    Update login page

--- src/pages/bookTicket/BookTicket.jsx
+++ src/pages/bookTicket/BookTicket.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useNavigate} from 'react-router-dom';
 
 const BookTicket = () => {
-  
-  return (
-    <div>BookTicket</div>
-  )
+
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    if(!localStorage.getItem('USER_LOGIN')){
+      return navigate("/login")
+  }
+  },[])
+return (
+  <div>BookTicket</div>
+)
 }
 
 export default BookTicket
--- src/services/quanLyNguoiDungService.js
+++ src/services/quanLyNguoiDungService.js
@@ -4,7 +4,4 @@ export const quanLyNguoiDungService = {
     postUser: (data) => {
       return api.post("QuanLyNguoiDung/DangNhap", data);
     },
-    getMovieDetail: (idFilm) => {
-      return api.get(`QuanLyPhim/LayThongTinPhim?MaPhim=${idFilm}`);
-    },
 };
