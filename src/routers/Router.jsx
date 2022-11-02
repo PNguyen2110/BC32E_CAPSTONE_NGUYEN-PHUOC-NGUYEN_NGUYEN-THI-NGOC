a615890785620a4cf085993d5c7b089fd357c48d
PNguyen2110 <nphuocnguyen997@gmail.com>
2022-11-02 20:32:45 +0700

    push code

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,58 +1,58 @@
-import React from 'react'
-import { Navigate, useRoutes } from 'react-router-dom'
-import MainLayout from '../components/layouts/MainLayout'
-import Contact from '../pages/contact/Contact'
-import Home from '../pages/home/Home'
-import News from '../pages/news/News'
-import Login from '../pages/login/Login'
-import Register from '../pages/register/Register'
-import MovieDetail from '../pages/movieDetail/MovieDetail'
-import BookTicket from '../pages/bookTicket/BookTicket'
+import React from "react";
+import { Navigate, useRoutes } from "react-router-dom";
+import MainLayout from "../components/Layouts/MainLayout";
+// import MainLayout form '../components/Layouts/MainLayout'
+import Contact from "../pages/contact/Contact";
+import Home from "../pages/home/Home";
+import News from "../pages/news/News";
+import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
+import MovieDetail from "../pages/movieDetail/MovieDetail";
+import BookTicket from "../pages/bookTicket/BookTicket";
 
 const Router = () => {
   const routing = useRoutes([
     {
-      path: '/',
+      path: "/",
       element: <MainLayout />,
       children: [
         {
-          path: '',
-          element: <Navigate to='home' />
+          path: "",
+          element: <Navigate to="home" />,
         },
 
         {
-          path: 'home',
-          element: <Home />
+          path: "home",
+          element: <Home />,
         },
         {
-          path: 'contact',
-          element: <Contact />
+          path: "contact",
+          element: <Contact />,
         },
         {
-          path: 'news',
-          element: <News />
+          path: "news",
+          element: <News />,
         },
         {
-          path: 'login',
-          element: <Login />
+          path: "login",
+          element: <Login />,
         },
         {
-          path: 'register',
-          element: <Register />
+          path: "register",
+          element: <Register />,
         },
         {
-          path:'detail/:movieIds',
-          element: <MovieDetail/>
+          path: "detail/:movieIds",
+          element: <MovieDetail />,
         },
         {
-          path: 'ticketroom/:id',
-          element: <BookTicket />
-        }
-      ]
-    }
-  ])
-  return routing
+          path: "ticketroom/:id",
+          element: <BookTicket />,
+        },
+      ],
+    },
+  ]);
+  return routing;
+};
 
-}
-
-export default Router
+export default Router;
