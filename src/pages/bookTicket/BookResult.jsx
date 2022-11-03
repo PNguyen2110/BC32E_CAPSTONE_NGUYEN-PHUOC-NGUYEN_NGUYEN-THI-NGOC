56ea84a61a4e0927a353d6884ef857241c123533
NgocNguyen1998 <ngocnguyen09498@gmail.com>
2022-11-03 19:25:14 +0700

    update bookTicket reasult vs set sign in or user name

--- src/pages/bookTicket/BookResult.jsx
+++ src/pages/bookTicket/BookResult.jsx
@@ -2,16 +2,17 @@ import moment from 'moment';
 import React from 'react'
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getBookResult, postBookResult, useQuanLyNguoiDung } from '../../storeToolKit/quanLyNguoiDung';
+import { getBookResult, useQuanLyNguoiDung } from '../../storeToolKit/quanLyNguoiDung';
 import _ from 'lodash'
 import { Fragment } from 'react';
 import { Skeleton } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { RollbackOutlined } from '@ant-design/icons';
 const BookResult = () => {
 
-  const { inFoUser,userLogin,isFetchinginFoUser } = useQuanLyNguoiDung()
-  console.log("inFoUser: ", inFoUser);
+  const { inFoUser,isFetchinginFoUser } = useQuanLyNguoiDung()
   const dispatch = useDispatch()
-  
+  const navigate = useNavigate()
   useEffect(()=>{
     dispatch(getBookResult()) 
   },[])
@@ -21,7 +22,7 @@ const BookResult = () => {
       <div className='grid grid-cols-3 gap-9 container'>
       {
          [...Array(10)].map(e=>{
-          return <div className='col-span-1 mt-4' >
+          return <div className='col-span-1 mt-4' key={Math.floor(Math.random() * 1000000) + 100000}>
             <Skeleton.Button active block style={{height:'350px'}}/>
           </div>
          })
@@ -32,7 +33,7 @@ const BookResult = () => {
   
   return (
     <div >
-      <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font bg-pink-100">
         <div className="container px-5 py-32 mx-auto">
           <div className="flex flex-col text-center w-full mb-12">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-purple-600">Booking history</h1>
@@ -40,18 +41,18 @@ const BookResult = () => {
           </div>
           <div className="flex flex-wrap -m-2">
 
-            {inFoUser.thongTinDatVe?.map((ticket,index)=>{
-              return   <div className="p-2 lg:w-1/3 md:w-1/2 w-full" key={index}>
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+            {inFoUser.thongTinDatVe?.map((ticket)=>{
+              return   <div className="p-2 lg:w-1/3 md:w-1/2 w-full" key={ticket.maVe}>
+              <div className="h-full flex items-center border-pink-400 border p-4 rounded-lg bg-white">
                 <img alt="phim" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={ticket.hinhAnh} />
                 <div className="flex-grow">
                   <h2 className="text-gray-900 title-font font-medium">{ticket.tenPhim}</h2>
                   <p className="text-gray-500"><i className='font-semibold'>Time:</i> {moment(ticket.ngayDat).format('hh:mm A ')} - <i className='font-semibold'>Date:</i>  {moment(ticket.ngayDat).format('DD-MM-YYYY') }</p>
                   <p><i className='font-semibold'>Address:</i> {_.first(ticket.danhSachGhe).tenHeThongRap}</p>
                   <p><i className='font-semibold'>Tên Rạp:</i> {_.first(ticket.danhSachGhe).tenCumRap}</p> 
-                  <p> <i className='font-semibold'>Ghế:</i> {ticket.danhSachGhe.map((ghe,index)=>{
+                  <p> <i className='font-semibold'>Ghế: </i> {ticket.danhSachGhe.map((ghe,index)=>{
                      return <Fragment>
-                       <span key={index}>{ghe.tenGhe} , </span>
+                       <span key={ghe.maGhe} className="text-green-400">[{ghe.tenGhe}] , </span>
                        {(index + 1) % 20 === 0 ? <br /> : ''}
                      </Fragment>
                      })}</p>
@@ -62,6 +63,12 @@ const BookResult = () => {
             })}
           
           </div>
+          <div className='text-right text-xl mt-6 pt-9'>
+          <a className="text-red-500 inline-flex items-center  py-2 px-4 rounded-lg  hover:text-black cursor-pointer bg-purple-200 hover:bg-white" onClick={() => navigate(-1)}>Back to booking page
+          <RollbackOutlined className='pl-2' />
+        </a>
+          </div>
+          
         </div>
       </section>
     </div>
